273b5b1c1f8ef41d98c9068027ffd38d44128d71
HCS <464962683@qq.com>
2017-12-23 23:44:38 +0800

    更新了 拖尾粒子

---
+++ Assets/Particles/Test02/CannonBallScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallScript : MonoBehaviour
+{
+    public float destoryTime = 1;
+
+	void Start ()
+    {
+        Destroy(gameObject, destoryTime);
+	}
+
+}
---
+++ Assets/Particles/Test02/CannonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour
+{
+    public GameObject ball;
+
+    public float shootForce;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject go = Instantiate(ball, transform.position, transform.rotation);
+            go.GetComponent<Rigidbody>().AddForce(shootForce*go.transform.forward);
+        }
+    }
+}
