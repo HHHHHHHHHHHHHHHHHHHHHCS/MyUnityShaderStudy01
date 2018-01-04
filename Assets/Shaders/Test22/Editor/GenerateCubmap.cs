3616f2b4d9932541702205d064dff52baafc0d5c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-01-04 16:44:32 +0800

    环境映射 学习

---
+++ Assets/Shaders/Test22/Editor/GenerateCubmap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GenerateCubmap : ScriptableWizard
+{
+    public Transform renderFromPosition;
+
+    public Cubemap cubemap;
+
+    private void OnWizardUpdate()
+    {
+        helpString = "Select transform to render from and cube mao to render into";
+        isValid = (renderFromPosition != null) && (cubemap != null);
+    }
+
+    private void OnWizardCreate()
+    {
+        GameObject go = new GameObject("Cubemap Camera", typeof(Camera));
+        go.transform.position = renderFromPosition.position;
+        go.transform.rotation = Quaternion.identity;
+
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+        DestroyImmediate(go);
+    }
+
+    [MenuItem("Tool/Render into Cubemap")]
+    private static void RenderCubemap()
+    {
+        DisplayWizard("Render cubemao", typeof(GenerateCubmap), "Render");
+    }
+}
---
+++ Assets/Shaders/Test22/S_Test22.shader
@@ -0,0 +1,32 @@
+﻿Shader "Custom/S_Test22"
+{
+	Properties
+	{
+		_MainTex("Base texture", 2D) = "white"
+		_Cubemap("Env map", CUBE) = "" {}
+	}
+	SubShader
+	{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 200
+		CGPROGRAM
+		#pragma surface surf Lambert
+		sampler2D _MainTex;
+		samplerCUBE _Cubemap;
+		struct Input
+		{
+			float2 uv_MainTex;
+			float3 worldRefl;
+		};
+		void surf(Input IN, inout SurfaceOutput o)
+		{
+			fixed4 c = tex2D(_MainTex, IN.uv_MainTex);
+			o.Albedo = c.rgb;
+			fixed4 reflcol = texCUBE(_Cubemap, IN.worldRefl);
+			reflcol *= c.a;
+			o.Emission = reflcol.rgb;
+			o.Alpha = reflcol.a;
+		}
+		ENDCG
+	}
+}
