0ee97c1260f060f5d66535e1c934f64ad1ddf45a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-01-17 15:51:58 +0800

    顶点颜色学习

---
+++ Assets/Shaders/Test25/MeshColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshColor : MonoBehaviour
+{
+
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        Vector3[] vertices = mesh.vertices;
+        Color[] colors = new Color[vertices.Length];
+        for(int i = 0;i<vertices.Length;i++)
+        {
+            colors[i] =Random.ColorHSV();
+        }
+        //colors[0] = Color.red;
+        //colors[1] = Color.green;
+        //colors[2] = Color.blue;
+        //colors[3] = Color.magenta;
+        mesh.colors = colors;
+    }
+}
---
+++ Assets/Shaders/Test25/S_Test25.shader
@@ -0,0 +1,38 @@
+﻿Shader "Custom/S_Test25"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)",2D) = "white"{}
+	}
+
+	SubShader
+	{
+		Tags{"RenderType" = "Opaque"}
+		LOD 200
+
+		CGPROGRAM
+		#pragma surface surf Lambert vertex:vert
+
+		sampler2D _MainTex;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+			float4 vertColor;
+		};
+
+		void vert(inout appdata_full v, out Input o)
+		{
+			UNITY_INITIALIZE_OUTPUT(Input,o);
+			o.vertColor = v.color;
+		}
+
+		void surf(Input IN, inout SurfaceOutput o)
+		{
+			o.Albedo = IN.vertColor.rgb*tex2D(_MainTex, IN.uv_MainTex).rgb;
+		}
+
+		ENDCG
+	}
+		FallBack "Diffuse"
+}
