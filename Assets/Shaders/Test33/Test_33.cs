99b771f5e4cbe9b052cb5f078c1be79b16fe3916
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-03-26 14:48:11 +0800

    外轮廓 和代码 设置SHADER

---
+++ Assets/Shaders/Test33/Test_33.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_33 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.SetFloat("_OutlineFactor", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
